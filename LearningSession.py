c5c52ff071c55d9606664fbe0f47373e9014bfda
Ching Fang <chingfang17@gmail.com>
2019-10-25 13:35:49 -0400

    Fills in LearningSession to load in relevant variables correctly

--- LearningSession.py
+++ LearningSession.py
@@ -1,6 +1,8 @@
 import os
 import pickle
 import csv
+import h5py
+import re
 import numpy as np
 from copy import deepcopy
 from scipy.io import loadmat
@@ -10,8 +12,8 @@ class Session(object):
     Represents one task session.
     """
     
-    datadir = "/home/chingf/Code/Widefield/data/musall/"
-    analysisdir = "/home/chingf/Code/Widefield/analysis/musall/"
+    datadir = "/home/chingf/Code/Widefield/data/musall/learning/neural"
+    analysisdir = "/home/chingf/Code/Widefield/analysis/musall/learning/"
     dirpath = None
     task = None
     mouse = None
@@ -19,32 +21,32 @@ class Session(object):
     num_trials = 0
     num_bins = 0
     num_components = 0
-    neural = {}
+    Vc = {}
     trialmarkers = {}
+    spatialdisc = {}
     
     def __init__(
             self, task, mouse, date, access_engram=False
             ):
         """
         Args
-            task: String; "vistrained" or "audiotrained"
             mouse: String; the name of the mouse
             date: String; the date of the session
         Raises
             ValueError: if the inputs do not specify a valid filepath.
         """
         
-        self.task = task
         self.mouse = mouse
         self.date = date
         if access_engram:
-            self.datadir = "/home/chingf/engram/data/musall/"
-            self.analysisdir = "/home/chingf/engram/analysis/behavenet/musall/"
+            self.datadir = "/home/chingf/engram/data/musall/learning/neural"
+            self.analysisdir = "/home/chingf/engram/analysis/musall/learning"
         self.dirpath = self.datadir + task + "/" + mouse + "/" + date + "/"
         if not os.path.isdir(self.dirpath):
             raise ValueError("Invalid path: " + self.dirpath)
-        self._load_neural()
+        self._load_Vc()
         self._load_trialmarkers()
+        self._load_spatialdisc()
         
     def get_delay_period(self, include_stim=False):
         """
@@ -67,32 +69,17 @@ class Session(object):
             delay_periods.append([delay_start, delay_end])
         return np.array(delay_periods)
 
-    def get_lever_grab_activity(self):
-        """ Extracts the activity centered around the lever grab initializing the
-        trial. This will always be at frame 54. Activity will be extracted from
-        half a second before the lever grab (15 frames) to a second after the
-        lever grab (30 frames)
-
-        Returns
-            A (trials, 15+30, components) numpy array.
+    def _load_Vc(self):
+        """
+        Loads the `Vc.mat` file and saves its data structures.
         """
 
-        levergrab_frame = 54
-        start_frame = levergrab_frame - 0 
-        end_frame = levergrab_frame + 30
-        return self.neural['neural'][:,start_frame:end_frame,:]
-
-    def _load_neural(self):
-        """
-        Loads the `neural.mat` file and saves its data structures.
-        """
-
-        filepath = self.dirpath + "neural.mat"
-        matfile = loadmat(filepath)
-        for key in [key for key in matfile if not key.startswith("__")]:
-            self.neural[key] = np.array(matfile[key]).squeeze()
+        filepath = self.dirpath + "Vc.mat"
+        matfile = h5py.File(filepath, 'r')
+        for key in matfile:
+            self.Vc[key] = np.array(matfile[key]).squeeze()
         self.num_trials, self.num_bins, self.num_components =\
-            self.neural['neural'].shape
+            self.Vc['Vc'].shape
 
     def _load_trialmarkers(self):
         """
@@ -103,6 +90,23 @@ class Session(object):
         matfile = loadmat(filepath)
         for key in [key for key in matfile if not key.startswith("__")]:
             self.trialmarkers[key] = np.array(matfile[key]).squeeze()
+
+    def _load_spatialdisc(self):
+        """
+        Loads the relevant data structures from Spatial Disc.
+        """
+
+        pattern = '(\d{2})-(\w{3})-(\d{4})'
+        date_pattern = re.search(pattern, self.date)
+        day = date_pattern.group(1)
+        month = date_pattern.group(2)
+        year = date_pattern.group(2)
+        filename = self.mouse + '_SpatialDisc_' + month + day +\
+            '_' + year + '_Session2.mat'
+        filepath = self.dirpath + filename
+        matfile = loadmat(filepath)
+        sessiondata = np.array(matfile['SessionData'])[0,0]
+        self.spatialdisc['Unassisted'] = sessiondata['Assisted']
 
     def _get_delay_period_trial(self, trial_num, include_stim=False):
         """
@@ -119,9 +123,20 @@ class Session(object):
             A tuple corresponding to the time bin boundaries of the delay period.
             The first value is inclusive, the second value is exclusive.
         """
-        
-        delay_start = self.trialmarkers['stimTime'][trial_num]
-        if not include_stim:
-            delay_start += 51
-        delay_end = self.trialmarkers['spoutTime'][trial_num]
+       
+        if self.trialmarkers['CorrectSide'] == 1: # Left
+            if self.trialmarkers['audStimL'][0,trial_num].size > 0: # Auditory 
+                stims = self.trialmarkers['audStimL'][0,trial_num]
+            else: # Tactile
+                stims = self.trialmarkers['tacStimL'][0,trial_num]
+        elif self.trialmarkers['CorrectSide'] == 2: # Right
+            if self.trialmarkers['audStimR'][0,trial_num].size > 0: # Auditory 
+                stims = self.trialmarkers['audStimR'][0,trial_num]
+            else: # Tactile
+                stims = self.trialmarkers['tacStimR'][0,trial_num]
+        if include_stim:
+            delay_start = np.min(stims) - 1
+        else:
+            delay_start = np.max(stims) - 1
+        delay_end = self.trialmarkers['spoutTime'][0,trial_num]
         return (delay_start, delay_end)
